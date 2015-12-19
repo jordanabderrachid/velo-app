75b115d9779d2f8826ad9c33bb73be80f02d8fe0
Jordan Abderrachid <jabderrachid@gmail.com>
2015-12-19 20:29:25 +0100

    Use `$http` service to fetch stations data.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -4,28 +4,28 @@ angular.module('velo-app').controller('MainCtrl', ['$scope', 'apiService', funct
     $scope.initialize = function () {
         var map = L.mapbox.map('map', 'mapbox.streets');
 
-        var apiRequest = new XMLHttpRequest();
+        apiService.getStations().then(
+            function (response) {
+                var stations = response.data;
 
-        apiRequest.open('GET', '/api/cities/lyon/stations');
+                var stationMarkers = [];
 
-        apiRequest.addEventListener('load', function () {
-            var stations = JSON.parse(this.response);
+                stations.forEach(function (station) {
+                    var stationMarker = L.marker(L.latLng(station.position.lat, station.position.lng));
 
-            var stationMarkers = [];
+                    stationMarkers.push(stationMarker);
+                });
 
-            stations.forEach(function (station) {
-                var stationMarker = L.marker(L.latLng(station.position.lat, station.position.lng));
-
-                stationMarkers.push(stationMarker);
-            });
-
-            var group = L.featureGroup(stationMarkers);
-            group.addTo(map);
-            
-            map.fitBounds(group.getBounds());
-        });
-
-        apiRequest.send();
+                var group = L.featureGroup(stationMarkers);
+                group.addTo(map);
+                
+                map.fitBounds(group.getBounds());
+            },
+            function (error) {
+                // TODO handle this error properly.
+                console.error(error);
+            }
+        );
     };
     
     $scope.initialize();
